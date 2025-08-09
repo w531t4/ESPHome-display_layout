0181ccc71e94f058ab591c6bca1cf91a6e319afa
aaron <w531t4@gmail.com>
2025-08-09 09:46:26 -0400

    move hi/current/low temp badge into header

--- include/ui_badges.h
+++ include/ui_badges.h
@@ -4,7 +4,31 @@
 #include "esphome/components/font/font.h"
 #include "esphome/components/display/display.h"
 
+using esphome::Color;
+Color TEAL   (0x00, 0xCE, 0xD1); //  0, 206, 209
+Color BLUE   (0x7B, 0x68, 0xEE); // 123, 104, 238
+Color BROWN  (0xD2, 0x69, 0x1E); // 210, 105,  30
+Color ORANGE (0xFF, 0xA5, 0x00); // 255, 165,   0
+Color GREEN  (0x00, 0x80, 0x00); //   0, 128,   0
+Color PINK   (0xFF, 0x69, 0xB4); // 255, 105, 180
+Color PURPLE (0x94, 0x00, 0xD3); // 148,   0, 211
+Color RED    (0xFF, 0x00, 0x00); // 255,   0,   0
+Color YELLOW (0xFF, 0xFF, 0x00); // 255, 255,   0
+
 namespace ui {
+
+inline void draw_hi_current_low_temp(esphome::display::Display &it,
+                                     esphome::font::Font *font,
+                                     int x,
+                                     int y,
+                                     float high,
+                                     float current,
+                                     float low
+                                     ) {
+     it.printf(x, y + (11*0), font, RED,  "%.0f", high);
+     it.printf(x, y + (11*1), font, TEAL, "%.0f", current);
+     it.printf(x, y + (11*2), font, BLUE, "%.0f", low);
+};
 
 // Draw a rounded orange badge with centered white text (like HA updates)
 inline void draw_badge(esphome::display::Display &it,
