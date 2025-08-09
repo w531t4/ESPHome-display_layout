e40183a68e7919fcdb52dceb89c6dd38fd39241d
aaron <w531t4@gmail.com>
2025-08-09 10:35:53 -0400

    move month/day widget into header

---
+++ include/ui_datetime.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "esphome/components/display/display.h"
+#include "esphome/components/image/image.h"
+#include "ui_colors.h"
+
+namespace ui {
+
+inline void draw_monthday(esphome::display::Display &it,
+                          esphome::font::Font *font_month,
+                          esphome::font::Font *font_day,
+                          int x, int y,
+                          auto now) {
+    char month_text[4];
+    now.strftime(month_text, sizeof(month_text), "%b");  // e.g., "Jun"
+    std::string month_string(month_text);
+    std::transform(month_string.begin(), month_string.end(), month_string.begin(), ::toupper); // JUN
+
+    it.printf(  x + 10 + (5 * 0), y, font_month, PINK, "%c", month_string[0]); // J
+    it.printf(  x + 10 + (5 * 1), y, font_month, PINK, "%c", month_string[1]); // U
+    it.printf(  x + 10 + (5 * 2), y, font_month, PINK, "%c", month_string[2]); // N
+    it.strftime(x               , y, font_day, PURPLE, "%d", now); // 08 <<- day
+};
+} // end namespace ui
