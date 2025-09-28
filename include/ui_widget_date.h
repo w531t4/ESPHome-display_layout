a0660481dca833e4c9ce6dbbd9808a8f2a90f9e0
Aaron White <w531t4@gmail.com>
2025-09-28 15:17:39 -0400

    add month to date widget

--- include/ui_widget_date.h
+++ include/ui_widget_date.h
@@ -7,6 +7,7 @@
 #include "ui_widget.h"
 #include "ui_widgetcomposite.h"
 #include "ui_widget_numeric.h"
+#include "ui_widget_string.h"
 #include "ui_shared.h"
 #include "ui_colors.h"
 
@@ -22,20 +23,34 @@ namespace ui {
         void initialize(const InitArgs& a) override {
             // std::array<std::unique_ptr<Widget>, 3> members;
             CompositeWidget<2>::initialize(a);
+            if (!a.font2.has_value()) {
+                ESP_LOGE("widget_date","Required font2 parameter missing\n");
+                return;
+            }
+            if (!*a.font2) {
+                ESP_LOGE("widget_date", "Required font2 must not be nullptr\n");
+                return;
+            }
             members[0] = std::make_unique<NumericWidget<uint8_t,3>>(); // DAY
             members[0]->initialize(InitArgs{.it = a.it, .anchor = a.anchor, .font = a.font, .font_color = PURPLE, .fmt = std::string("%02d")});
+            members[1] = std::make_unique<StringWidget<4>>(); // MONTH
+            members[1]->initialize(InitArgs{.it = a.it, .anchor = ui::Coord(anchor.x + 10 + 2, anchor.y), .font = *a.font2, .font_color = PINK});
             // members[1] = std::make_unique<NumericWidget<float,float_bufsize>>(); // CURRENT
             // members[1]->initialize(InitArgs{.it = a.it, .anchor = ui::Coord(a.anchor.x, a.anchor.y + (11*1)), .font = a.font, .font_color = TEAL});
             // members[2] = std::make_unique<NumericWidget<float,float_bufsize>>(); // LOW
             // members[2]->initialize(InitArgs{.it = a.it, .anchor = ui::Coord(a.anchor.x, a.anchor.y + (11*2)), .font = a.font, .font_color = BLUE});
             initialized = true;
         }
-
+        const std::string get_month(const uint8_t month) {
+            if (month < 1 || month > 12) return std::string("UNK");
+            return std::string(months[month-1]);
+        }
         void post(const PostArgs& args) {
             if (args.extras.has_value()) {
                 const DatePostArgs *post_args_ptr = std::any_cast<const DatePostArgs>(&args.extras);
                 if (post_args_ptr != nullptr) {
                     members[0]->post(PostArgs{.extras = ui::NumericPostArgs<uint8_t>{.value = post_args_ptr->day}});
+                    members[1]->post(PostArgs{.extras = ui::StringPostArgs{.value = get_month(post_args_ptr->month)}});
                     // const std::size_t n = std::min(members.size(), post_args_ptr->values.size());
                     // for (std::size_t i = 0; i < n; i++) {
                     //     if (members[i]) {
