ffb02de0d257536fb0f67cf25d3ae45b698ceb05
Aaron White <w531t4@gmail.com>
2025-10-05 13:58:41 -0400

    create network tput widget

---
+++ include/ui_widget_network_tput.h
@@ -0,0 +1,41 @@
+// MyDerived.hpp
+#pragma once
+#include <iostream>
+#include <array>
+#include <span>
+#include "ui_widget.h"
+#include "ui_widgetcomposite.h"
+#include "ui_widget_text_numeric.h"
+#include "ui_shared.h"
+#include "ui_colors.h"
+
+namespace ui {
+    struct NetworkTputPostArgs {
+        float rx;
+        float tx;
+    };
+    class NetworkTputWidget : public CompositeWidget<2> {
+    public:
+        void initialize(const InitArgs& a) override {
+            CompositeWidget<2>::initialize(a);
+            constexpr int float_bufsize = 8; // "1000 TX\0"
+            members[0] = std::make_unique<NumericWidget<float,float_bufsize>>(); // HIGH
+            members[0]->initialize(InitArgs{.it = a.it, .anchor = ui::Coord(a.anchor.x, a.anchor.y),
+                                            .font = a.font, .font_color = RED,  .fmt = std::string("%4.0f TX")});
+            members[1] = std::make_unique<NumericWidget<float,float_bufsize>>(); // CURRENT
+            members[1]->initialize(InitArgs{.it = a.it, .anchor = ui::Coord(a.anchor.x, a.anchor.y + 11),
+                                            .font = a.font, .font_color = TEAL, .fmt = std::string("%4.0f RX")});
+            initialized = true;
+        }
+
+        void post(const PostArgs& args) {
+            if (args.extras.has_value()) {
+                const NetworkTputPostArgs *post_args_ptr = std::any_cast<const NetworkTputPostArgs>(&args.extras);
+                if (post_args_ptr != nullptr) {
+                    members[0]->post(PostArgs{.extras = ui::NumericPostArgs<float>{.value = post_args_ptr->tx}});
+                    members[1]->post(PostArgs{.extras = ui::NumericPostArgs<float>{.value = post_args_ptr->rx}});
+                }
+            }
+        }
+    };
+}
