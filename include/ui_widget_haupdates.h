3fb9a8905629d496d49edc33b35e1cf33018f59e
Aaron White <w531t4@gmail.com>
2025-10-13 17:58:28 -0400

    create skeleton ha_updates widget

---
+++ include/ui_widget_haupdates.h
@@ -0,0 +1,46 @@
+// MyDerived.hpp
+#pragma once
+#include <iostream>
+#include <array>
+#include <span>
+#include "argsbag.h"
+#include "ui_widget.h"
+#include "ui_widgetcomposite.h"
+#include "ui_widget_text_numeric.h"
+#include "ui_shared.h"
+#include "ui_colors.h"
+
+namespace ui {
+    struct HAUpdatesPostArgs {
+        int value;
+    };
+    class HAUpdatesWidget : public CompositeWidget<1> {
+    public:
+        void initialize(const InitArgs& a) override {
+            CompositeWidget<1>::initialize(a);
+            constexpr int bufsize = 3; // "1000 TX\0"
+            members[0] = std::make_unique<NumericWidget<int,bufsize>>(); // HIGH
+            members[0]->initialize(InitArgs{.it = a.it, .anchor = ui::Coord(a.anchor.x, a.anchor.y),
+                                            .font = a.font, .font_color = RED,  .fmt = std::string("%d"),
+                                            .extras = ArgsBag::of(TextInitArgs{.right_align = true})});
+            initialized = true;
+        }
+
+        void post(const PostArgs& args) {
+            if (args.extras.has_value()) {
+                const HAUpdatesPostArgs *post_args_ptr = std::any_cast<const HAUpdatesPostArgs>(&args.extras);
+                if (post_args_ptr != nullptr) {
+                    members[0]->post(PostArgs{.extras = ui::NumericPostArgs<int>{.value = post_args_ptr->value}});
+                    if (post_args_ptr->value == 0) {
+                        if (this->is_enabled()) {
+                            this->blank();
+                            this->enabled = false;
+                        }
+                    } else {
+                        if (!this->is_enabled()) this->enabled = true;
+                    }
+                }
+            }
+        }
+    };
+}
