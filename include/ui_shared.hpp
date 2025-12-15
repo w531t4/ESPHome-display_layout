dcb09689af5b8434c4a9352b8e2af31a55928028
Aaron White <w531t4@gmail.com>
2025-12-15 09:27:43 -0500

    trim unused legacy implementation

--- include/ui_shared.hpp
+++ include/ui_shared.hpp
@@ -2,11 +2,7 @@
 #include "esphome/components/display/display.h"
 #include "esphome/components/font/font.h"
 #include "esphome/components/homeassistant/text_sensor/homeassistant_text_sensor.h"
-#include "ui_colors.hpp"
 #include <algorithm>
-#include <limits>
-#include <optional>
-#include <vector>
 
 namespace ui {
 
@@ -18,48 +14,6 @@ struct Coord {
     int y;
     constexpr Coord(int x_, int y_) : x{x_}, y{y_} {}
 };
-inline bool box_valid(const Box &b) { return b.w > 0 && b.h > 0; }
-inline int box_x2(const Box &b) { return b.x1 + b.w; } // exclusive
-inline int box_y2(const Box &b) { return b.y1 + b.h; } // exclusive
-
-inline std::optional<Box> enclosing_box(const std::vector<Box> &boxes) {
-    int min_x = std::numeric_limits<int>::max();
-    int min_y = std::numeric_limits<int>::max();
-    int max_x = std::numeric_limits<int>::min();
-    int max_y = std::numeric_limits<int>::min();
-    bool any = false;
-
-    for (const auto &b : boxes) {
-        if (!box_valid(b))
-            continue;
-        any = true;
-        if (b.x1 < min_x)
-            min_x = b.x1;
-        if (b.y1 < min_y)
-            min_y = b.y1;
-        if (box_x2(b) > max_x)
-            max_x = box_x2(b);
-        if (box_y2(b) > max_y)
-            max_y = box_y2(b);
-    }
-
-    if (!any)
-        return std::nullopt;
-
-    Box out;
-    out.x1 = min_x;
-    out.y1 = min_y;
-    out.w = max_x - min_x; // exclusive -> width
-    out.h = max_y - min_y; // exclusive -> height
-    return out;
-}
-inline std::optional<Box> enclosing_box(std::initializer_list<Box> list) {
-    return enclosing_box(std::vector<Box>(list));
-}
-
-inline std::optional<Box> enclosing_box(const Box &a, const Box &b) {
-    return enclosing_box({a, b});
-}
 
 inline void mywipe(esphome::display::Display *it, Box &prev_box,
                    esphome::Color blank_color) {
@@ -124,78 +78,6 @@ inline void myprint(esphome::display::Display *it, esphome::font::Font *font,
     it->get_text_bounds(x, y, buf, font, align, &x1, &y1, &w, &h);
     it->printf(x, y, font, font_color, align, "%s", buf);
     prev_box = {x1, y1, w, h};
-}
-inline void wipe_and_print(esphome::display::Display *it,
-                           esphome::font::Font *font, int x, int y, char *buf,
-                           esphome::display::TextAlign align,
-                           esphome::Color blank_color,
-                           esphome::Color font_color, Box &prev_box) {
-    mywipe(it, prev_box, blank_color);
-    myprint(it, font, x, y, buf, align, font_color, prev_box);
-}
-
-// must create unique (see below) when used
-// struct LowTempTag {};
-template <typename Tag>
-inline void
-clean_draw_float(esphome::display::Display &it, esphome::font::Font *font,
-                 int x, int y, float value, esphome::Color font_color,
-                 esphome::Color blank_color, esphome::display::TextAlign align,
-                 const std::string fmtstring = "%0.f", const int bufsize = 4) {
-    static float last = -400.0;
-    static Box prev_box;
-
-    if (value == last)
-        return;
-
-    char buf[bufsize];
-    snprintf(buf, sizeof(buf), fmtstring.c_str(), value);
-    last = value;
-
-    wipe_and_print(&it, font, x, y, buf, align, blank_color, font_color,
-                   prev_box);
-}
-
-template <typename Tag>
-inline void
-clean_draw_string(esphome::display::Display &it, esphome::font::Font *font,
-                  int x, int y, const std::string value,
-                  esphome::Color font_color, esphome::Color blank_color,
-                  esphome::display::TextAlign align,
-                  const std::string fmtstring = "%s", const int bufsize = 4) {
-    static std::string last = "preset value";
-    static Box prev_box;
-
-    if (value == last)
-        return;
-
-    char buf[bufsize];
-    snprintf(buf, sizeof(buf), fmtstring.c_str(), value.c_str());
-    last = value;
-
-    wipe_and_print(&it, font, x, y, buf, align, blank_color, font_color,
-                   prev_box);
-}
-
-template <typename Tag>
-inline void clean_draw_int(
-    esphome::display::Display &it, esphome::font::Font *font, int x, int y,
-    const int value, esphome::Color font_color,
-    esphome::display::TextAlign align = esphome::display::TextAlign::LEFT,
-    esphome::Color blank_color = BLACK, const std::string fmtstring = "%0.f",
-    const int bufsize = 4, const int init_last_value = -1) {
-    static int last = init_last_value;
-    static Box prev_box;
-
-    if (value == last)
-        return;
-
-    char buf[bufsize];
-    snprintf(buf, sizeof(buf), fmtstring.c_str(), value);
-    last = value;
-
-    wipe_and_print(&it, font, x, y, buf, align, blank_color, font_color,
-                   prev_box);
 }
 
 inline bool txt_sensor_has_healthy_state(
--- include/ui_weather.hpp
+++ include/ui_weather.hpp
@@ -1,8 +1,5 @@
 #pragma once
-#include "esphome/components/display/display.h"
 #include "esphome/components/image/image.h"
-#include "ui_colors.hpp"
-#include "ui_shared.hpp"
 #include <string>
 #include <unordered_map>
 
@@ -28,48 +25,5 @@ inline bool is_night_hour(int hour, int night_start = 21, int night_end = 6) {
     // Night if hour >= night_start OR hour < night_end (wraps midnight)
     return (hour >= night_start) || (hour < night_end);
 }
-
-// The following contains a post which outlines the possible weather states
-// https://community.home-assistant.io/t/met-no-weather-condition-possible-states/405353/6
-inline void draw_weather_image(esphome::display::Display &it,
-                               const Coord anchor, const std::string &state,
-                               int this_hour, int night_start = 21,
-                               int night_end = 6) {
-    const int img_width = 32, img_height = 32;
-    static std::string last_state = "junk_state";
-    if (last_state == state)
-        return;
-
-    auto itf = icon_registry().find(state);
-    if (itf == icon_registry().end())
-        return;
-
-    const bool night = is_night_hour(this_hour, night_start, night_end);
-    esphome::image::Image *img = night ? itf->second.night : itf->second.day;
-    if (!img)
-        return;
-
-    it.image(anchor.x, anchor.y, img, esphome::display::COLOR_ON,
-             esphome::display::COLOR_OFF); // draw
-    last_state = state;
-}
-
-struct HighTempTag {};
-struct CurrentTempTag {};
-struct LowTempTag {};
-inline void draw_hi_current_low_temp(esphome::display::Display &it,
-                                     esphome::font::Font *font,
-                                     const Coord anchor, float high,
-                                     float current, float low) {
-    clean_draw_float<HighTempTag>(it, font, anchor.x, anchor.y + (11 * 0), high,
-                                  RED, BLACK,
-                                  esphome::display::TextAlign::LEFT);
-    clean_draw_float<CurrentTempTag>(it, font, anchor.x, anchor.y + (11 * 1),
-                                     current, TEAL, BLACK,
-                                     esphome::display::TextAlign::LEFT);
-    clean_draw_float<LowTempTag>(it, font, anchor.x, anchor.y + (11 * 2), low,
-                                 BLUE, BLACK,
-                                 esphome::display::TextAlign::LEFT);
-};
 
 } // namespace ui
