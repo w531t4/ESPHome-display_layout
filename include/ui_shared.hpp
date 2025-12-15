70300a778b96c5dfd9425d11d7278fa6611454db
Aaron White <w531t4@gmail.com>
2025-12-15 09:10:28 -0500

    add missing header

--- include/ui_shared.hpp
+++ include/ui_shared.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "esphome/components/display/display.h"
+#include "esphome/components/homeassistant/text_sensor/homeassistant_text_sensor.h"
 #include "ui_colors.hpp"
 #include <algorithm>
 #include <limits>
