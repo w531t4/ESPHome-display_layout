7f4379ef23f60e7eac78621055a852726d064983
Aaron White <w531t4@gmail.com>
2025-12-25 16:32:19 +0000

    move maps

---
+++ components/display_layout/config/maps.py
@@ -0,0 +1,20 @@
+# SPDX-FileCopyrightText: 2025 Aaron White <w531t4@gmail.com>
+# SPDX-License-Identifier: MIT
+MAGNET_MAP = {
+    "left": "Magnet::LEFT",
+    "right": "Magnet::RIGHT",
+    "auto": "Magnet::AUTO",
+}
+
+WIDGET_TYPE_MAP = {
+    "twitch_icons": "display_layout::WidgetKind::TWITCH_ICONS",
+    "twitch_chat": "display_layout::WidgetKind::TWITCH_CHAT",
+    "pixel_motion": "display_layout::WidgetKind::PIXEL_MOTION",
+    "network_tput": "display_layout::WidgetKind::NETWORK_TPUT",
+    "weather": "display_layout::WidgetKind::WEATHER",
+    "temperatures": "display_layout::WidgetKind::TEMPERATURES",
+    "date": "display_layout::WidgetKind::DATE",
+    "time": "display_layout::WidgetKind::TIME",
+    "ha_updates": "display_layout::WidgetKind::HA_UPDATES",
+    "psn": "display_layout::WidgetKind::PSN",
+}
