34cac83ac181fc29fd002e3b1a39263515c10fed
Aaron White <w531t4@gmail.com>
2026-01-14 02:44:32 +0000

    refactor:rename base_widget_text_string.hpp

--- components/display_layout/ui_widget_time.hpp
+++ components/display_layout/ui_widget_time.hpp
@@ -5,7 +5,7 @@
 #include "ui_colors.hpp"
 #include "ui_shared.hpp"
 #include "base_widget_text_numeric.hpp"
-#include "ui_widget_text_string.hpp"
+#include "base_widget_text_string.hpp"
 #include "base_widget_composite.hpp"
 #include <array>
 
